3986b0df606b08e651dea7aa1e88dd388bf122dc
potykion <potykion@gmail.com>
2023-01-05 13:43:53 +0300

    :construction: vuetify

--- potyk-ci-front/src/main.ts
+++ potyk-ci-front/src/main.ts
@@ -7,10 +7,19 @@ import router from './router'
 
 import './assets/main.css'
 
-const app = createApp(App)
+// Vuetify
+import 'vuetify/styles'
+import {createVuetify} from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-app.use(createPinia())
-app.use(router)
-app.use(plugin, defaultConfig({theme: 'genesis'}))
+const vuetify = createVuetify({
+    components,
+    directives,
+})
 
-app.mount('#app')
+createApp(App)
+    .use(createPinia())
+    .use(router)
+    .use(vuetify)
+    .mount('#app')
